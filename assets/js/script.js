7ae435d16fd1c05a5555ba619cbb237b9ba32df8
C A <chadandari@outlook.com>
2023-12-18 15:50:59 -0500

    Added rough HTML outline, some CSS, answer pool in Js

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+var questionBank = {
+    question1: "Arrays in Javascript can be used to store _________.",
+    answer1a: "numbers and strings",
+    answer1b: "other arrays",
+    answer1c: "booleans",  
+    answer1d: "all of the above",
+    correctAnswer1: answer1d,
+
+    question2: "String values must be enclosed within _________ when being assigned to variables.",
+    answer2a: "commas",
+    answer2b: "curly brackets",
+    answer2c: "quotes",  
+    answer2d: "parenthesis",
+    correctAnswer2: answer2c,
+
+    question3: "The condition in an if/else statement is enclosed with _________.",
+    answer3a: "quotes",
+    answer3b: "curly brackets",
+    answer3c: "parentesis",  
+    answer3d: "square brackets",
+    correctAnswer3: answer3c,
+    
+    question4: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer4a: "JavaScript",
+    answer4b: "terminal/bash",
+    answer4c: "for loops",  
+    answer4d: "console.log",
+    correctAnswer4: answer4d,
+
+    question5: "Commonly used data types do NOT include:",
+    answer5a: "strings",
+    answer5b: "booleans",
+    answer5c: "alerts",  
+    answer5d: "numbers",
+    correctAnswer5: answer5c,
+};
+
